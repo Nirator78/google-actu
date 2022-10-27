80de1901f6b1b88be1341417e21eb3aa167783f7
Nirator78 <51956876+Nirator78@users.noreply.github.com>
2022-10-27 15:44:00 +0200

    commentaire test argument

--- test_Argument.py
+++ test_Argument.py
@@ -1,13 +1,19 @@
+"""
+Test de la classe Argument
+"""
 from utils.Argument import Argument
 import sys
 
 # py .\test_Argument.py 1 2
 
+# On initialise la classe Argument avec les arguments de la ligne de commande
 arg = Argument(sys.argv)
 
+# On récupère les arguments
 print(arg.recherche())
 print(arg.nombrePage())
 
+# On initiliase la classe Argument sans arguments pour voir les valeurs par défaut
 argEmpty = Argument([])
 
 print(argEmpty.recherche())
