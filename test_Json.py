e83abb91c9b2903b70de03a1b162196ca2e16527
Nirator78 <51956876+Nirator78@users.noreply.github.com>
2022-10-27 15:41:15 +0200

    commentaire test json

--- test_Json.py
+++ test_Json.py
@@ -1,13 +1,22 @@
+"""
+Test de la classe Json
+"""
 from utils.Json import Json
 
+# On initiliase la classe Json
 json = Json('test.json')
 
+# On récupère le contenu du fichier
 print(json.getJson())
 
+# On insert un contenu dans le fichier
 json.setJson([{'test': 'test'}])
 
+# On récupère le contenu du fichier
 print(json.getJson())
 
+# On supprime le contenu du fichier
 json.cleanJson()
 
+# On récupère le contenu du fichier
 print(json.getJson())
